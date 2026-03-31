2f97bdec55852aafffb46a2974b496bd98a63a8a
aalLuxen <aholybiolux@gmail.com>
2026-03-30 23:48:21 -0400

    fix button and navigate in same tab

--- script.js
+++ script.js
@@ -6,14 +6,20 @@ const menuOverlay = document.getElementById('menu-overlay');
 menuOpen.addEventListener('click', () => {
     mainNav.style.display = 'flex';
     menuOverlay.style.display = 'block';
+    menuOpen.setAttribute('aria-expanded', 'true');
+    mainNav.setAttribute('aria-hidden', 'false');
 })
 
 menuClose.addEventListener('click', () => {
     mainNav.style.display = 'none';
     menuOverlay.style.display = 'none';
+    menuOpen.setAttribute('aria-expanded', 'false');
+    mainNav.setAttribute('aria-hidden', 'true');
 })
 
 menuOverlay.addEventListener('click', () => {
     mainNav.style.display = 'none';
     menuOverlay.style.display = 'none';
+    menuOpen.setAttribute('aria-expanded', 'false');
+    mainNav.setAttribute('aria-hidden', 'true');
 })
